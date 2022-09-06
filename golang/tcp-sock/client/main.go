31552e55511b56d217aade108d2752e0ba2c0723
fuleyi <fuleyi@uniontech.com>
2022-09-06 08:59:10 +0800

    tcp 拥塞控制调试

--- golang/tcp-sock/client/main.go
+++ golang/tcp-sock/client/main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"math/rand"
 	"net"
-	"strconv"
 )
+
+var letters = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+
+func randBytes(n int) []byte {
+	b := make([]byte, n)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return b
+}
+
+func sendManual(conn net.Conn) {
+	defer conn.Close()
+	for {
+		fmt.Printf("请输入发送[次数，长度, 0-*退出]：")
+		var times, len int
+		fmt.Scanln(&times, &len)
+		if times == 0 {
+			return
+		}
+		for i := 0; i < times; i++ {
+			buff := randBytes(len)
+			_, err := conn.Write(buff)
+			if err != nil {
+				log.Println("send err:", err)
+				return
+			}
+			log.Println("send success:", string(buff))
+		}
+	}
+}
 
 func main() {
 	conn, err := net.Dial("tcp", "127.0.0.1:29999")
 	if err != nil {
 		log.Fatalln("Client dail err,", err)
 	}
-	defer conn.Close()
-	for i := 0; i < 1; i++ {
-		var buff string = "hello world [" + strconv.Itoa(i) + "]"
-		_, err := conn.Write([]byte(buff))
-		if err != nil {
-			log.Println("send err:", err)
-			return
-		}
-		log.Println("send success:", buff)
-	}
+	go sendManual(conn)
 	select {}
 }
--- golang/tcp-sock/server/main.go
+++ golang/tcp-sock/server/main.go
@@ -2,10 +2,39 @@ package main
 
 import (
 	"bufio"
+	"fmt"
 	"io"
 	"log"
 	"net"
 )
+
+// 处理：手动接受报文
+// 户口端发送n多个报文，server端手动接受报文，查看缓冲区和tcp连接的窗口变化
+// netstat -anpt |grep <port>
+func processManual(conn net.Conn) {
+	defer conn.Close()
+	for {
+		fmt.Printf("请输入接收次数，长度(0 * 退出)：")
+		var times, len int
+		fmt.Scanln(&times, &len)
+		if times == 0 {
+			return
+		}
+		for i := 0; i < times; i++ {
+			var buf []byte = make([]byte, len)
+			n, err := conn.Read(buf[:])
+			if err != nil {
+				if err.Error() == io.EOF.Error() {
+					return
+				}
+				log.Fatalln("Tcp server read err,", err)
+			}
+			recvStr := string(buf[:n])
+			log.Println("接收到客户端数据：", recvStr)
+		}
+	}
+	select {}
+}
 
 func process(conn net.Conn) {
 	defer conn.Close()
@@ -32,11 +61,12 @@ func main() {
 		log.Fatalln("Tcp listen err,", err)
 	}
 	for {
-		_, err := listen.Accept()
+		conn, err := listen.Accept()
 		log.Println("New accetp...")
 		if err != nil {
 			log.Fatalln("Tcp accept err,", err)
 		}
 		//go process(conn)
+		go processManual(conn)
 	}
 }
