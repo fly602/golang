ed0c3c8885308e1d842c4a43da20e731077f2a70
fuleyi <fuleyi@uniontech.com>
2022-11-14 17:33:19 +0800

    channel 场景测试

--- golang/channel/main.go
+++ golang/channel/main.go
@@ -1,25 +1,35 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"time"
 )
 
 func main() {
-	taskChan := make(chan int, 100)
+	var taskChan = make(chan int, 100)
 
 	go func() {
-		for i := 0; i < 100; i++ {
+		for i := 0; i < 10; i++ {
 			taskChan <- i
 		}
 	}()
 	go func() {
-		time.Sleep(time.Second * 10)
+		time.Sleep(time.Second * 3)
 		close(taskChan)
 	}()
-	for task := range taskChan {
-		fmt.Println(task)
-		log.Println("")
+
+	f1 := func(i int) {
+		for task := range taskChan {
+			log.Println("go", i, " =", task)
+		}
 	}
+
+	go f1(1)
+	f1(2)
+
+	// 给已关闭的管道发送数据，会崩溃
+	// taskChan <- 10
+
+	task, ok := <-taskChan
+	log.Println("Last get task=", task, "ok =", ok)
 }
