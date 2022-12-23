3dbd767ba434c97d750750018373b3982037366d
fuleyi <fuleyi@uniontech.com>
2022-12-23 17:13:40 +0800

    添加docker 微服务mall,使用的是go-zero框架

---
+++ docker/mall/user/api/internal/logic/registerlogic.go
@@ -0,0 +1,30 @@
+package logic
+
+import (
+	"context"
+
+	"go-community/docker/mall/user/api/internal/svc"
+	"go-community/docker/mall/user/api/internal/types"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type RegisterLogic struct {
+	logx.Logger
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+}
+
+func NewRegisterLogic(ctx context.Context, svcCtx *svc.ServiceContext) *RegisterLogic {
+	return &RegisterLogic{
+		Logger: logx.WithContext(ctx),
+		ctx:    ctx,
+		svcCtx: svcCtx,
+	}
+}
+
+func (l *RegisterLogic) Register(req *types.RegisterRequest) (resp *types.RegisterResponse, err error) {
+	// todo: add your logic here and delete this line
+
+	return
+}
