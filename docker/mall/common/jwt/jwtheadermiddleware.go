9272d80dec4eca6b9212a13d630340a779b03faa
fuleyi <fuleyi@uniontech.com>
2023-01-07 23:18:12 +0800

    jwt修改自动续期逻辑

--- docker/mall/common/jwt/jwtheadermiddleware.go
+++ docker/mall/common/jwt/jwtheadermiddleware.go
@@ -2,7 +2,6 @@ package jwtx
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 	"time"
 )
@@ -17,20 +16,34 @@ func NewJwtheaderMiddleware(j JwtAuth) *JwtheaderMiddleware {
 	}
 }
 
+// 此中间件需要放在jwt之后
 func (m *JwtheaderMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO generate middleware implement function, delete after code implementation
+		// 获取jwt解析后的数据
+		uid, _ := r.Context().Value("uid").(json.Number).Int64()
+		old, _ := r.Context().Value("uid").(json.Number).Int64()
+
 		exp := m.Auth.AccessExpire
 		now := time.Now().Unix()
-		uid, _ := r.Context().Value("uid").(json.Number).Int64()
-		fmt.Printf("exp=%+v,now=%+v,uid=%+v", exp, now, uid)
-		accessToken, err := GetToken(m.Auth.AccessSecret, now, exp, uid)
-		if err != nil {
-			next(w, r)
-			return
+
+		var accessToken string
+		var err error
+		if now-old < exp/2 {
+			accessToken, err = GetToken(m.Auth.AccessSecret, now, exp, uid)
+			if err != nil {
+				next(w, r)
+				return
+			}
+		} else {
+			authorization := r.Header["Authorization"]
+			if authorization != nil {
+				accessToken = authorization[0]
+			}
 		}
 		// Passthrough to next handler if need
 		w.Header().Add("Authorization", accessToken)
+
 		// Passthrough to next handler if need
 		next(w, r)
 	}
