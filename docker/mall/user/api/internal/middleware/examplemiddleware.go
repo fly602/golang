bce641f31e447b7a190baf8fcca61f52433b9f86
fuleyi <fuleyi@uniontech.com>
2023-01-07 22:20:36 +0800

    1.修复中间件报错问题 2.调整api和rpc服务权限

--- docker/mall/user/api/internal/middleware/examplemiddleware.go
+++ docker/mall/user/api/internal/middleware/examplemiddleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"encoding/json"
 	"fmt"
 	"go-community/docker/mall/user/api/internal/config"
 	jwtx "go-community/docker/mall/user/common/jwt"
@@ -21,11 +22,9 @@ func NewExampleMiddleware(c config.Config) *ExampleMiddleware {
 func (m *ExampleMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO generate middleware implement function, delete after code implementation
-		// exp := r.Context().Value("exp")
-		// logger := logx.WithContext(r.Context())
 		exp := m.Config.Auth.AccessExpire
 		now := time.Now().Unix()
-		uid := r.Context().Value("uid").(int64)
+		uid, _ := r.Context().Value("uid").(json.Number).Int64()
 		fmt.Printf("exp=%+v,now=%+v,uid=%+v", exp, now, uid)
 		accessToken, err := jwtx.GetToken(m.Config.Auth.AccessSecret, now, exp, uid)
 		if err != nil {
