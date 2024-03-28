cdc1efe631aabd1bfa244c55a2fbac8631aaa3c5
fuleyi <fuleyi@uniontech.com>
2024-03-28 16:50:47 +0800

    完善k8s环境配置脚本

--- 总结/k8s环境创建/run.sh
+++ 总结/k8s环境创建/run.sh
@@ -205,7 +205,7 @@ install_fannel(){
   if [ -e "kube-flannel.yml" ];then
     wget -O kube-flannel.yml https://github.com/flannel-io/flannel/releases/download/v0.24.3/kube-flannel.yml
   fi
-  kubectl apply -f ./kube-flannel.yml
+  sudo -u $USER_NAME kubectl apply -f ./kube-flannel.yml
 }
 
 init_k8s(){
